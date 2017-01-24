5499360b5b0a0b4ff675290aa85374695d933688
Anthony Duperray <duperran@duperran-macbookair.roam.corp.google.com>
2017-01-24 17:15:22 +0100

    lab2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,27 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ListitemsComponent } from './listitems/listitems.component';
+import { ItemService } from './service/item.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
+    ListitemsComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     RouterModule.forRoot([
-      {path:'', component: HomeComponent}
+      {path:'', component: HomeComponent},
+      {path:'items', component: ListitemsComponent}
 
       ])
   ],
-  providers: [],
+  providers: [ItemService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
